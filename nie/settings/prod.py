16604ac208f6091a00682007dcaccd9411bfe2fb
Dominik Szmek <do@minik.pl>
2018-09-08 14:44:47 +0200

    add allowed hosts

--- nie/settings/prod.py
+++ nie/settings/prod.py
@@ -2,4 +2,8 @@ from .base import * # noqa F403
 
 DEBUG = False
 
-ALLOWED_HOSTS += ['172.104.129.103', 'test.domino.nieruchomosci.pl'] # noqa F405
+ALLOWED_HOSTS += [  # noqa F405
+    '172.104.129.103',
+    'test.domino.nieruchomosci.pl',
+    'domino.nieruchomosci.pl',
+]
